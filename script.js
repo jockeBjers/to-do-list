38060e5b02dc1e14b4666c6bb3b11541b078b549
Joakim Bjerselius <176925453+jockeBjers@users.noreply.github.com>
2024-11-01 16:01:28 +0100

    add delete button for each list item

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function turnBlue() {
-    this.classList.toggle("blue");
-}
+const buttons = document.querySelectorAll('.clicky');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        this.classList.toggle("blue");
+    });
+});
 
 const container = document.querySelector(".todo-div");
 const input = document.querySelector(".todo-list");
@@ -8,20 +12,43 @@ const button = document.querySelector(".btn-list");
 const ul = document.querySelector(".ul-list");
 
 function addItem() {
-    const text = input.value.trim();
-    if (text) {
+    const text = input.value;
+    if (text == '') {
+        alert("No empty messages!")
+    } else {
         const li = document.createElement("li");
         li.textContent = text;
         li.classList.add("todo-text");
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("delete-btn")
+        deleteBtn.textContent = "X";
+
+           deleteBtn.addEventListener("click", () => {
+            li.remove();
+           }) 
+
+        li.appendChild(deleteBtn);
         ul.appendChild(li);
-        input.value = ""; 
+        input.value = '';
     }
 }
 
+/* add item event */
 button.addEventListener("click", addItem);
 
 input.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
         addItem();
+    }
+});
+
+/* Reset all */
+const resetBtn = document.querySelector(".remove-btn");
+
+resetBtn.addEventListener("click", () => {
+    input.value = '';
+    while (ul.lastElementChild) {
+        ul.removeChild(ul.lastElementChild);
     }
 });
